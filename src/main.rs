c2c28695a5c0c8588dbbf9fe90af3b0d801ebe2d
MarcoIeni <11428655+MarcoIeni@users.noreply.github.com>
2024-08-30 10:49:50 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 mod cmd;
 
-use camino::Utf8PathBuf;
+use camino::{Utf8Path, Utf8PathBuf};
 use cmd::Cmd;
 use git_cmd::Repo;
 
 fn main() -> anyhow::Result<()> {
-    let repo = get_repo();
+    let repo_root = repo_root();
+    let repo = Repo::new(repo_root.clone()).unwrap();
     let commit_message = inquire::Text::new("Commit message").prompt().unwrap();
     anyhow::ensure!(
         !commit_message.is_empty() && commit_message.len() < 71,
@@ -20,27 +21,40 @@ fn main() -> anyhow::Result<()> {
         .prompt()
         .unwrap();
 
-    let staged_files = get_staged_files();
+    let staged_files = get_staged_files(&repo_root);
     println!("ℹ️ Staged files: {:?}", staged_files);
-    Cmd::new("git-town", ["hack", &branch_name]).run();
+    Cmd::new("git-town", ["hack", &branch_name])
+        .with_current_dir(&repo_root)
+        .run();
 
     if staged_files.is_empty() {
-        run_git_add(changed_files(&repo));
+        run_git_add(changed_files(&repo), repo.directory());
     } else {
-        run_git_add(staged_files);
+        run_git_add(staged_files, &repo_root);
     }
 
-    let output = Cmd::new("git", ["commit", "-m", &commit_message]).run();
+    let output = Cmd::new("git", ["commit", "-m", &commit_message])
+        .with_current_dir(&repo_root)
+        .run();
     if output.stdout().contains("nothing to commit") {
         panic!("❌ Nothing to commit");
     }
 
-    Cmd::new("git-town", ["propose"]).run();
+    Cmd::new("git-town", ["propose"])
+        .with_current_dir(&repo_root)
+        .run();
     Ok(())
 }
 
-fn get_staged_files() -> Vec<Utf8PathBuf> {
-    let output = Cmd::new("git", ["diff", "--name-only", "--cached"]).run();
+fn repo_root() -> Utf8PathBuf {
+    let git_root = Cmd::new("git", ["rev-parse", "--show-toplevel"]).run();
+    camino::Utf8PathBuf::from(git_root.stdout())
+}
+
+fn get_staged_files(curr_dir: &Utf8Path) -> Vec<Utf8PathBuf> {
+    let output = Cmd::new("git", ["diff", "--name-only", "--cached"])
+        .with_current_dir(curr_dir)
+        .run();
     output.stdout().lines().map(Utf8PathBuf::from).collect()
 }
 
@@ -52,19 +66,15 @@ fn changed_files(repo: &Repo) -> Vec<Utf8PathBuf> {
     changed_files.iter().map(|f| git_root.join(f)).collect()
 }
 
-fn get_repo() -> git_cmd::Repo {
-    let current_dir = std::env::current_dir().unwrap();
-    let current_dir = camino::Utf8PathBuf::from_path_buf(current_dir).unwrap();
-    git_cmd::Repo::new(current_dir).unwrap()
-}
-
-fn run_git_add(changed_files: Vec<Utf8PathBuf>) {
+fn run_git_add(changed_files: Vec<Utf8PathBuf>, repo_root: &Utf8Path) {
     assert!(!changed_files.is_empty(), "No files to add");
     let mut git_add_args = vec!["add".to_string()];
     let changed_files: Vec<String> = changed_files.iter().map(|f| f.to_string()).collect();
     git_add_args.extend(changed_files);
 
-    Cmd::new("git", &git_add_args).run();
+    Cmd::new("git", &git_add_args)
+        .with_current_dir(repo_root)
+        .run();
 }
 
 fn branch_name_from_commit_message(commit_message: &str) -> String {
